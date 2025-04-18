41748017d5fdda41deab0322b26c7aa259f982b3
NguyenXTung <xtung781@gmail.com>
2025-04-18 19:11:56 +0700

    Update main.cpp

--- LingoGame/main.cpp
+++ LingoGame/main.cpp
@@ -229,7 +229,6 @@ void setup(SDL_Window* window, SDL_Renderer* renderer){
     SDL_RenderPresent(renderer);
     nhaptudien();
     keyword = randomkeyword(ROW);
-    cerr << keyword << endl;
     beginword = "";
     for(int i = 0; i < ROW; i++){
         beginword += ".";
